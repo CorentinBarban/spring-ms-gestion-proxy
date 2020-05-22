d619476d0dcbd5481f3abf69f692f6eb4805e643
cbarban <corentin.barban@sfr.fr>
2020-05-22 10:50:22 +0200

    Cors Policy

--- src/main/java/com/barban/corentin/m2/miagesousleau/proxy/config/GatewayConfiguration.java
+++ src/main/java/com/barban/corentin/m2/miagesousleau/proxy/config/GatewayConfiguration.java
@@ -1,9 +1,15 @@
 package com.barban.corentin.m2.miagesousleau.proxy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -12,10 +18,12 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable();
+        http.cors().and().authorizeRequests()
                 .antMatchers("/oauth/**")
                 .permitAll()
                 .antMatchers("/**")
                 .authenticated();
     }
+
 }
