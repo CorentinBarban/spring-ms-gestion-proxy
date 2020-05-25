047a6c632bff3c729258c5b9bb3c40241c823563
cbarban <corentin.barban@sfr.fr>
2020-05-25 16:25:25 +0200

    Add informations into token

--- src/main/java/com/barban/corentin/m2/miagesousleau/proxy/config/GatewayConfiguration.java
+++ src/main/java/com/barban/corentin/m2/miagesousleau/proxy/config/GatewayConfiguration.java
@@ -1,15 +1,11 @@
 package com.barban.corentin.m2.miagesousleau.proxy.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 
 
 @Configuration
